6b08cd8a1073967bed332f3a333f83e543f00a46
Sota Yokoe <sota@kreuz45.com>
2018-05-20 23:42:00 +0900

    refactor: Adding semicolons.

--- ts/eth/qr-code-reader.ts
+++ ts/eth/qr-code-reader.ts
@@ -1,23 +1,23 @@
 class QRCodeReader {
-  public onPreview: (video: any) => void
-  public onData: (data: string) => void
-  public jsQR: (data: any, width: number, height: number) => any
+  public onPreview: (video: any) => void;
+  public onData: (data: string) => void;
+  public jsQR: (data: any, width: number, height: number) => any;
 
   private readingQRCode: boolean = false;
-  private video: any
-  private canvas: any
-  private onStarted: () => void
+  private video: any;
+  private canvas: any;
+  private onStarted: () => void;
   private isStarted: boolean = false;
 
   constructor(jsQR: any, video: any, canvas: any) {
-    this.jsQR = jsQR
-    this.video = video
-    this.canvas = canvas
+    this.jsQR = jsQR;
+    this.video = video;
+    this.canvas = canvas;
   }
 
   public start = (onStarted: () => void, onError: (error) => void) => {
-    this.onStarted = onStarted
-    const this_ = this
+    this.onStarted = onStarted;
+    const this_ = this;
     navigator.mediaDevices.getUserMedia({ video: { facingMode: "environment" } }).then((stream) => {
       this_.video.srcObject = stream;
       this_.video.setAttribute("playsinline", true); // required to tell iOS safari we don't want fullscreen
@@ -26,7 +26,7 @@ class QRCodeReader {
       this_.isStarted = false;
       requestAnimationFrame(this_.tick);
     }).catch((e) => {
-      onError(e)
+      onError(e);
     });
   }
 
@@ -41,14 +41,14 @@ class QRCodeReader {
     if (this.video.readyState === this.video.HAVE_ENOUGH_DATA) {
       if (!this.isStarted) {
         this.isStarted = true;
-        this.onStarted()
+        this.onStarted();
       }
-      this.onPreview(this.video)
+      this.onPreview(this.video);
 
       const imageData = this.canvas.getImageData(0, 0, this.video.videoHeight, this.video.videoWidth);
       const code = this.jsQR(imageData.data, imageData.width, imageData.height);
       if (code != null && this.readingQRCode) {
-        this.onData(code.data)
+        this.onData(code.data);
       }
     }
     if (this.readingQRCode) {
