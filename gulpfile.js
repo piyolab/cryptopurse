e6a9fc9d1b099263c46d7d4d81f16e2565efec90
Sota Yokoe <sota@kreuz45.com>
2018-05-20 23:45:16 +0900

    chore: Install tslint.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var replace = require('gulp-replace');
 var git = require('git-rev');
 var pkg = require('./package.json')
 var typescript = require('gulp-typescript');
+var tslint = require("gulp-tslint");
 const webpackStream = require('webpack-stream');
 const webpack = require('webpack');
 const webpackConfig = require("./webpack.config");
@@ -17,7 +18,8 @@ var paths = {
   'html': './public/',
   'js': './public/js/',
   'css': './public/css/',
-  'src': './src/'
+  'src': './src/',
+  'ts': './ts/'
 }
 
 //setting : Pug Options
@@ -67,15 +69,25 @@ gulp.task('ts', () => {
   var ethOptions =  {
      out: 'ether-tools.js'
   };
-  gulp.src(['./ts/eth/*.ts', '!./node_modules/**'])
+  gulp.src([paths.ts + '**/*.ts', '!./node_modules/**'])
     .pipe(typescript(ethOptions))
     .pipe(gulp.dest(paths.js));
  });
 
+//tslint
+gulp.task("tslint", () =>
+  gulp.src([paths.ts + '**/*.ts', '!./node_modules/**'])
+    .pipe(plumber({ errorHandler: notify.onError("Error: <%= error.message %>") }))
+    .pipe(tslint({
+       formatter: "verbose"
+    }))
+    .pipe(tslint.report())
+);
+
 //watch
 gulp.task('watch', function () {
   gulp.watch([paths.pug + '**/*.pug', '!' + paths.pug + '**/_*.pug'], ['pug']);
-  gulp.watch(['./ts/eth/*.ts'], ['ts']);
+  gulp.watch([paths.ts + '**/*.ts'], ['tslint', 'ts']);
 });
 
 gulp.task('webpack', () => {
